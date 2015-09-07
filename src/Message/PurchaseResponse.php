411878057e21c9db44dc9292d08adbe6598f4d61
crynobone <crynobone@gmail.com>
2015-09-08 00:18:34 +0800

    Fixes invalid class name.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Message/PurchaseResponse.php
+++ src/Message/PurchaseResponse.php
@@ -3,7 +3,7 @@
 use Omnipay\Common\Message\AbstractResponse;
 use Omnipay\Common\Message\RedirectResponseInterface;
 
-class PurchaseResposne extends AbstractResponse implements RedirectResponseInterface
+class PurchaseResponse extends AbstractResponse implements RedirectResponseInterface
 {
     public function isSuccessful()
     {
